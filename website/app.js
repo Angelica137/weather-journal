40484fc1e95950d077d88a77e68a59373d6b083f
Angelica <Angelica137@users.noreply.github.com>
2024-01-21 19:28:29 +0000

    comment why you did not follow the instructions

--- website/app.js
+++ website/app.js
@@ -18,4 +18,12 @@ const getWeatherData = async (zipCode) => {
   }
 };
 
+// this callback does not have the base url or api key because
+// those live server side to try and keep separations of concerns
+// but I am not sure I did this right
+document.getElementById("generate").addEventListener("click", () => {
+  const zipCode = document.getElementById("zipCode").value;
+  getWeatherData(zipCode);
+});
+
 getWeatherData("94040");
